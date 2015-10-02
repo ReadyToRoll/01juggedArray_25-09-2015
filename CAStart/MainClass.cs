24d2391f584bed05d0b1a07b6e0a63c92ce9dbe0
cayman335@gmail.com <15021972q>
2015-10-02 07:43:01 +0300

    rewrited_project

---
+++ CAStart/MainClass.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace JaggedArray
+{
+    public class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Console.Out.WriteLine("Please input number of lines");
+            int number_of_lines = Convert.ToInt32(Console.In.ReadLine());
+
+            int[][] work_arr = new int[number_of_lines][];
+            init_arr(number_of_lines, work_arr);
+            input_array(number_of_lines, work_arr);
+
+            sortType ST = sortType.bySumOfValuesOfLine;
+            Console.Out.WriteLine("Your standard array:");
+            show_array(number_of_lines, work_arr);
+
+            Arr.sorting(work_arr, number_of_lines, true, ST);
+            Console.Out.WriteLine("Array is sorted by sum of values of line:");
+            show_array(number_of_lines, work_arr);
+
+            ST = sortType.byMinValueOfLine;
+            Arr.sorting(work_arr, number_of_lines, true, ST);
+            Console.Out.WriteLine("Array is sorted by min value of line:");
+            show_array(number_of_lines, work_arr);
+
+            ST = sortType.byMaxValueOfLine;
+            Arr.sorting(work_arr, number_of_lines, true, ST);
+            Console.Out.WriteLine("Array is sorted by max value of line:");
+            show_array(number_of_lines, work_arr);
+
+            Console.Out.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+        static void init_arr(int n, int[][] work_arr)
+        {
+            int a;
+            Console.Out.WriteLine("Please input dimension of first line");
+            for (int i = 0; i < n; i++)
+            {
+                a = Convert.ToInt32(Console.In.ReadLine());
+                work_arr[i] = new int[a];
+                if (i == n - 1)
+                    break;
+                Console.Out.WriteLine("Please input dimension of next line");
+            }
+        }
+
+        static void input_array(int n, int[][] work_arr)
+        {
+            Random rand_value;
+            int max_scd_dimension = 0;
+            for (int i = 0; i < n; i++)
+            {
+                max_scd_dimension = work_arr[i].Length;
+                for (int j = 0; j < max_scd_dimension; j++)
+                {
+                    rand_value = new Random(DateTime.Now.Millisecond);
+                    work_arr[i][j] = rand_value.Next(0, 25);
+                    System.Threading.Thread.Sleep(50);
+                }
+
+            }
+
+        }
+
+        static void show_array(int n, int[][] work_arr)
+        {
+            int max_scd_dimension = 0;
+            for (int i = 0; i < n; i++)
+            {
+                max_scd_dimension = work_arr[i].Length;
+                for (int j = 0; j < max_scd_dimension; j++)
+                {
+                    Console.Out.Write(work_arr[i][j] + " ");
+                }
+                Console.Out.WriteLine("");
+            }
+            Console.Out.WriteLine("");
+            Console.Out.WriteLine("");
+        }
+    }
+}
---
+++ LogicClass/Arr.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace JaggedArray
+{
+    public enum sortType { byMaxValueOfLine, byMinValueOfLine, bySumOfValuesOfLine };
+    public static class Arr
+    {
+        public static void sorting(int[][] work_arr, int n, bool ascending, sortType ST)
+        {
+            int max_dimension_1 = 0;
+            int max_dimension_2 = 0;
+            int first_value = 0, second_value = 0;
+            for (int k = 0; k < n; k++)
+                for (int i = n - 1; i > 0; i--)
+                {
+                    max_dimension_1 = work_arr[i].Length;
+                    max_dimension_2 = work_arr[i - 1].Length;
+                    switch (ST)
+                    {
+                        case sortType.byMaxValueOfLine:
+                            {
+                                if (ascending)
+                                {
+                                    if (max_value_in_array(work_arr[i]) > max_value_in_array(work_arr[i - 1]))
+                                        swap_lines(work_arr, max_dimension_1, max_dimension_2, i);
+                                }
+                                else if (max_value_in_array(work_arr[i]) < max_value_in_array(work_arr[i - 1]))
+                                    swap_lines(work_arr, max_dimension_1, max_dimension_2, i);
+                                break;
+                            }
+                        case sortType.byMinValueOfLine:
+                            {
+                                if (ascending)
+                                {
+                                    if (min_value_in_array(work_arr[i]) > min_value_in_array(work_arr[i - 1]))
+                                        swap_lines(work_arr, max_dimension_1, max_dimension_2, i);
+                                }
+                                else if (min_value_in_array(work_arr[i]) < min_value_in_array(work_arr[i - 1]))
+                                    swap_lines(work_arr, max_dimension_1, max_dimension_2, i);
+                                break;
+                            }
+                        case sortType.bySumOfValuesOfLine:
+                            {
+                                for (int j = 0; j < max_dimension_1; j++)
+                                    first_value += work_arr[i][j];
+                                for (int j = 0; j < max_dimension_2; j++)
+                                    second_value += work_arr[i - 1][j];
+                                if (ascending)
+                                {
+                                    if (second_value < first_value)
+                                        swap_lines(work_arr, max_dimension_1, max_dimension_2, i);
+                                }
+                                else if (second_value > first_value)
+                                    swap_lines(work_arr, max_dimension_1, max_dimension_2, i);
+                                first_value = 0;
+                                second_value = 0;
+                                break;
+                            }
+                    }
+
+                }
+        }
+
+        public static void swap_lines(int[][] work_arr, int max_dimension_1, int max_dimension_2, int i)
+        {
+            int temporary_dimension;
+            int[] first_mass = new int[1];
+            int[] second_mass = new int[1];
+            Array.Resize(ref first_mass, max_dimension_1);
+            Array.Resize(ref second_mass, max_dimension_2);
+            for (int j = 0; j < max_dimension_1; j++)
+                first_mass[j] = work_arr[i][j];
+            for (int j = 0; j < max_dimension_2; j++)
+                second_mass[j] = work_arr[i - 1][j];
+            temporary_dimension = work_arr[i].Length;
+            Array.Resize(ref work_arr[i], work_arr[i - 1].Length);
+            Array.Resize(ref work_arr[i - 1], temporary_dimension);
+            for (int j = 0; j < max_dimension_1; j++)
+                work_arr[i - 1][j] = first_mass[j];
+            for (int j = 0; j < max_dimension_2; j++)
+                work_arr[i][j] = second_mass[j];
+        }
+
+        public static int max_value_in_array(int[] arr)
+        {
+            int max = 0;
+            for (int i = 0; i < arr.Length; i++)
+                if (max < arr[i])
+                    max = arr[i];
+            return max;
+        }
+
+        public static int min_value_in_array(int[] arr)
+        {
+            int min = max_value_in_array(arr);
+            for (int i = 0; i < arr.Length; i++)
+                if (min > arr[i])
+                    min = arr[i];
+            return min;
+        }
+    }
+}
